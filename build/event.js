d29c33e7b64231b6c65c14950c5c98407f81be7d
Wilson Page <wilsonpage@me.com>
2013-04-07 17:45:19 +0100

    module.exports defined

--- build/event.js
+++ build/event.js
@@ -1,8 +1,8 @@
-(function(e){if("function"==typeof bootstrap)bootstrap("event",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeEvent=e}else"undefined"!=typeof window?window.Event=e():global.Event=e()})(function(){var define,ses,bootstrap,module,exports;
+(function(e){if("function"==typeof bootstrap)bootstrap("event",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeEvent=e}else"undefined"!=typeof window?window.event=e():global.event=e()})(function(){var define,ses,bootstrap,module,exports;
 return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0](function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
 
 
-function Event(obj) {
+var Event = module.exports = function(obj) {
 	if (!(this instanceof Event)) return new Event(obj);
 	if (obj) {
 		obj._cbs = {};
@@ -10,7 +10,7 @@ function Event(obj) {
 	}
 
 	this._cbs = {};
-}
+};
 
 
 Event.prototype.on = function(name, cb) {
@@ -25,6 +25,7 @@ Event.prototype.off = function(name, cb) {
 	if (!cb) return delete this._cbs[name];
 
 	cbs = this._cbs[name] || [];
+
 	while (cbs && ~(i = cbs.indexOf(cb))) cbs.splice(i, 1);
 };
 
